8404c2b528c9c88ef07b286a0986bb9c1fe2f950
1 <1>
2020-06-05 21:03:42 +0200

    damn

--- assets/remote_exec.sh
+++ assets/remote_exec.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 mkdir -p ~/.ssh
-echo $SSH_KEY > ~/.ssh/id_rsa.pub
+echo $SSH_KEY > ~/.ssh/id_rsa
 chmod 700 ~/.ssh
-chmod 600 ~/.ssh/id_rsa.pub
+chmod 600 ~/.ssh/id_rsa
 ssh-keyscan -H $IP_ADDR >> ~/.ssh/known_hosts
 ssh -t $DEPLOY_USER@$IP_ADDR 'bash -s' < ./deploy.sh

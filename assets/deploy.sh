7baf730b604ee12d20fc61f136c48d02eea65ad1
1 <1>
2020-06-05 19:58:14 +0200

    add travis deploy

---
+++ assets/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd /var/www/futurist;
+git pull origin master;
+
+cd frontend;
+yarn && yarn run build;
+
+cd ../backend;
+source venv/bin/activate;
+pip3 install -r requirements.txt;
+
+systemctl restart futurist-flask

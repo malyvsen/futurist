1f3ccea8036329cfe7da9c8c915eb48e6ddcc1a9
1 <1>
2020-06-05 23:22:19 +0200

    Fix perms v2

--- assets/remote_exec.sh
+++ assets/remote_exec.sh
@@ -7,4 +7,4 @@ ssh-add ./futurist_ci
 
 ssh-keyscan -H $IP_ADDR >> ~/.ssh/known_hosts
 
-ssh $DEPLOY_USER@$IP_ADDR "bash -c 'cd /var/www/futurist && ./assets/deploy.sh'"
+ssh $DEPLOY_USER@$IP_ADDR "bash -c 'cd /var/www/futurist && bash ./assets/deploy.sh'"

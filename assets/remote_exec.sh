cdf37b7466e57efeb49068a35d6a6f724de85bd1
1 <1>
2020-06-05 22:39:42 +0200

    add proper keys handling

--- assets/remote_exec.sh
+++ assets/remote_exec.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-mkdir -p ~/.ssh
-echo $SSH_KEY > ~/.ssh/id_rsa
-chmod 700 ~/.ssh
-chmod 600 ~/.ssh/id_rsa
+eval "$(ssh-agent -s)"
+
+chmod 600 ./futurist_ci
+ssh-add ./futurist_ci
+
 ssh-keyscan -H $IP_ADDR >> ~/.ssh/known_hosts
-ssh -t $DEPLOY_USER@$IP_ADDR 'bash -s' < ./deploy.sh
+
+ssh $DEPLOY_USER@$IP_ADDR "bash -c 'cd /var/www/futurist && ./assets/deploy.sh"

637225e41986130cbcdbde61de754324531315be
malyvsen <5940672+malyvsen@users.noreply.github.com>
2020-06-07 01:08:50 +0200

    fix test_parse to use beverage sales

--- backend/tests/test_parse.py
+++ backend/tests/test_parse.py
@@ -2,17 +2,11 @@ from pipeline.parse import parse
 
 
 
-def test_tsla():
-    parsed = parse('tsla.xls', '../examples/tsla.xls')
+def test_beverage():
+    parsed = parse('beverage-sales.xls', '../examples/beverage-sales.xls')
     assert parsed.index.name == 'date'
     assert 'datetime64' in str(parsed.index.dtype)
-    assert parsed.columns[0] == 'Open'
-    assert 'float' in str(parsed['Open'].dtype)
-    assert parsed.columns[1] == 'High'
-    assert 'float' in str(parsed['High'].dtype)
-    assert parsed.columns[2] == 'Low'
-    assert 'float' in str(parsed['Low'].dtype)
-    assert parsed.columns[3] == 'Close'
-    assert 'float' in str(parsed['Close'].dtype)
-    assert parsed.columns[4] == 'Volume'
-    assert 'int' in str(parsed['Volume'].dtype)
+    assert parsed.columns[0] == 'Lemonade sales'
+    assert 'float' in str(parsed['Lemonade sales'].dtype)
+    assert parsed.columns[1] == 'Beer sales'
+    assert 'float' in str(parsed['Beer sales'].dtype)

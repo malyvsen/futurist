c99990e98f135cb93b7db79c302457f2e7e1d887
1 <1>
2020-06-06 19:50:54 +0200

    Add indicators

--- frontend/src/ResultView.tsx
+++ frontend/src/ResultView.tsx
@@ -20,14 +20,26 @@ interface ResultViewProps {
 }
 
 const ResultView = ({ data }: ResultViewProps) => {
-  const elements = data.map((result) => (
+  const elements = data.map((result, index) => (
     <div key={result.name}>
-      <h1>{result.name}</h1>
+      <h2>{result.name}</h2>
+      {index === 0 && (
+        <p>
+          This plot lets you see into the future! Where the dates extend beyond today, the shaded
+          area indicates uncertainty.
+        </p>
+      )}
       <Plot {...result.plot} />
     </div>
   ));
 
-  return <Fragment>{elements}</Fragment>;
+  return (
+    <Fragment>
+      <h1>Your forecast is ready!</h1>
+      <p>Let's explore the dependencies.</p>
+      {elements}
+    </Fragment>
+  );
 };
 
 export { ResultView };
